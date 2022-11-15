fd0e3c5dce234ac1314ddcb577863f2f9ea4d9aa
JoaoMaciel5G <juauvitu765@gmail.com>
2022-11-14 22:23:04 -0300

    add auth users

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
 const express = require("express")
 const app = express()
 const http = require("http")
+const session = require("express-session")
 const server = http.createServer(app)
 const io = require("socket.io")(server)
-const arr = []
-app.use(express.static("public"))
+const nameUsers = []
+
+app.engine('html', require('ejs').renderFile);
+app.set("view engine", "html")
+app.set("views", __dirname + "/views")
+app.use(session({secret: "appUserSession", resave: true, saveUninitialized: true}))
 app.use(express.urlencoded({extended: true}))
 
-app.post("/name-verified", async function(request, response){
-    var nome = await request.body.nome
-    arr.push(nome)
-    if(!nome){
-        response.redirect("/index.html")
+app.post("/", function(request, response){
+    const nome = request.body.nome
+
+    if(nome){
+        nameUsers.push(nome)
+        request.session.name = nome
+        response.render("chat")
     }else{
-        response.redirect("/chat.html")
+        response.render("index")
     }
 })
+app.get("/", function(request, response){
+    response.render("index")
+})
+
+app.get("/styles/chat.css", function(request, response){
+    response.sendFile(__dirname + "/views/styles/chat.css")
+})
+app.get("/styles/index.css", function(request, response){
+    response.sendFile(__dirname + "/views/styles/index.css")
+})
+
 io.on("connection", function(socket){
     const name = arr[arr.length-1]
     socket.on("chat message", function(msg){
